15b9e8e1f89f9d1b45b3d4f5314109449064dff6
UbuBux <reia.koay@gmail.com>
2026-05-21 23:12:31 +1000

    Fixed cart overlay that wasn't appearing in product description. Also fixed quantity and trash button in cart.

--- product-desc.js
+++ product-desc.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(cartClose)
 
     // When cart icon is clicked, add class "open" to the overlay - this will make it visible
-    cartIcon.addEventListener("click", () => {
+    cartIcon.addEventListener("click", (event) => {
         event.preventDefault()
         cart.classList.add("open-cart")
     })
@@ -118,8 +118,106 @@ function showDetails() {
     document.getElementById("pro-img-1").src = product.image
     document.getElementById("product-name").textContent = product.name
     document.getElementById("product-price").textContent = "$" + product.price
+
+    let button = document.getElementById('addToCartBtn')
+    button.onclick = () => addToCart(productId)
 }
 
+// ADD ITEM TO CART //
+// empty array for products to be pushed // 
+// gets items from local storage (strings) and parses them into arrays again
+let cart = JSON.parse(localStorage.getItem("cart")) || []
+
+function addToCart(productId) {
+    // find product object id in products array and check whether the id matches the id of the button clicked
+    const product = products.find(item => item.id === productId)
+
+    // add product o the array
+    const existingItem = cart.find(item => item.id === productId)
+
+    if (existingItem) {
+        existingItem.quantity += 1
+    } else {
+        cart.push({
+            ...product,
+            quantity: 1
+        })
+    }
+
+    // convert array to string and store in local storage
+    localStorage.setItem("cart", JSON.stringify(cart))
+
+    // add to cart display
+    displayInCart()
+}
+
+//DISPLAY IN CART //
+function displayInCart() {
+
+    const cartContainer = document.querySelector("#display-cart-items")
+
+    // clear old cart HTML
+    cartContainer.innerHTML = ""
+
+    // loop through cart array
+    cart.forEach(item => {
+
+        cartContainer.innerHTML += `
+            <div class="cart-item">
+
+                <img src="${item.image}" width="100">
+
+                <div>
+                    <h3>${item.name}</h3>
+                    <p>$${item.price}</p>
+                </div>
+
+                <!-- Quantity -->
+                <ul id="quantity-buttons">
+                    <li><button class="increase-quan" onclick="increaseQuan(${item.id})">+</button></li>
+                    <li>${item.quantity}</li>
+                    <li><button class="decrease-quan" onclick="decreaseQuan(${item.id})">-</button></li>
+                </ul>
+
+                <!-- Trash -->
+                <button class="cart-trash" onclick="removeItem(${item.id})">trash</button>
+
+            </div>
+        `
+    })
+}
+
+// INCREASE AND DECREASE QUANTITY //
+function increaseQuan(id) {
+    const item = cart.find(product => product.id === id)
+    item.quantity += 1
+
+    saveCart()
+}
+
+function decreaseQuan(id) {
+    const item = cart.find(product => product.id === id)
+
+    item.quantity -= 1
+
+    if (item.quantity <= 0) {
+        cart = cart.filter(p => p.id !== id)
+    }
+
+    saveCart()
+}
+
+// SAVE CART INFO //
+function saveCart() {
+    localStorage.setItem("cart", JSON.stringify(cart))
+    displayInCart()
+}
+
+//REMOVE ITEMS WITH TRASH BUTTON //
+function removeItem(id) {
+    cart = cart.filter(p => p.id !== id)
+    saveCart()
+}
 
 
 /* element can be TR or TD
